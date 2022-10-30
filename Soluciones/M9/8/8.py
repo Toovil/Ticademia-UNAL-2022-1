a83130a2643ab0e900bf402fc5a1edfec1972cc1
Juan José Tobón <jujotovil@gmail.com>
2022-10-30 14:45:33 -0500

    Ejercicio 8

--- Soluciones/M9/8/8.py
+++ Soluciones/M9/8/8.py
@@ -1,6 +1,5 @@
 archivo = open("Soluciones/M9/8/mensaje.txt", "r")
 texto = ""
 for renglon in archivo:
-    print(texto.join(list(reversed(renglon))))
-
+    print(texto.join(list(reversed(renglon))).strip("\n"))
 archivo.close()
