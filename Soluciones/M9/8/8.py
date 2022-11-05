0583e58777f370f3d3c4cdaa0f09592bf8a1d28b
Juan José Tobón <jujotovil@gmail.com>
2022-11-04 21:06:06 -0500

    Soluciones M9 1-8

--- Soluciones/M9/8/8.py
+++ Soluciones/M9/8/8.py
@@ -1,4 +1,4 @@
-archivo = open("Soluciones/M9/8/mensaje.txt", "r")
+archivo = open("M9/8/mensaje.txt", "r")
 texto = ""
 for renglon in archivo:
     print(texto.join(list(reversed(renglon))).strip("\n"))
--- Soluciones/M9/prueba.py
+++
@@ -1,3 +0,0 @@
-texto = "hola"
-texto1= ""
-print(texto1.join(list(reversed(texto))))
